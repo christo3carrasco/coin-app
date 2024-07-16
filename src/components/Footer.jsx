854bd9ab8329b0afb2a8a04d15d45022835fad86
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-07-15 20:24:29 -0500

    feat: add Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
+import { useTheme } from "@emotion/react";
+import { Box, Typography } from "@mui/material";
+
 export const Footer = () => {
+  const theme = useTheme();
+
   return (
-    <footer>
-      <p>&copy; 2024 My App</p>
-    </footer>
+    <Box
+      component="footer"
+      sx={{
+        py: 2,
+        textAlign: "center",
+        bgcolor: theme.palette.background.default,
+        color: theme.palette.text.primary,
+      }}
+    >
+      <Typography variant="body2" color="inherit">
+        &copy; 2024 Coin App
+      </Typography>
+    </Box>
   );
 };
