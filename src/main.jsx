27f919c6796cfd34cea10f9c9a3813c79788ad1d
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-07-15 19:42:51 -0500

    feat: add material ui

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { CoinApp } from "./CoinApp.jsx";
+
+import { CssBaseline } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CssBaseline />
     <CoinApp />
   </React.StrictMode>
 );
