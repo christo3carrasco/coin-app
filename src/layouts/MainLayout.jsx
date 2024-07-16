b955ca5b21582585186431128b3b5b2662b33256
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-07-15 20:25:23 -0500

    feat: add main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
+import { Box } from "@mui/material";
 import { Footer, Header } from "../components";
 
 // eslint-disable-next-line react/prop-types
 export const MainLayout = ({ children }) => {
   return (
-    <div>
+    <Box display="flex" flexDirection="column" minHeight="100vh">
       <Header />
-      <main>{children}</main>
+      <Box component="main" flexGrow={1}>
+        {children}
+      </Box>
       <Footer />
-    </div>
+    </Box>
   );
 };
