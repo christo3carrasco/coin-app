2615e6621a990b1e705c35fd00f34471836108d7
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-07-15 20:24:43 -0500

    feat: add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
+import { AppBar, Button, styled, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+
+const LinkButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.common.white,
+  textDecoration: "none",
+}));
 
 export const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/coins">Coins</Link>
-          </li>
-          <li>
-            <Link to="/user">User</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          COIN APP
+        </Typography>
+        <LinkButton component={Link} to="/">
+          Home
+        </LinkButton>
+        <LinkButton component={Link} to="/coins">
+          Coins
+        </LinkButton>
+        <LinkButton component={Link} to="/user">
+          Users
+        </LinkButton>
+      </Toolbar>
+    </AppBar>
   );
 };
