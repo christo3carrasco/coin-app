deaced3ff0a14a84097e7b78d7743c883b17c469
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-07-16 02:24:53 -0500

    style: add space native / custom

--- src/hooks/useFetchCoins.js
+++ src/hooks/useFetchCoins.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+
 import { getCoins } from "../helpers/getCoins";
 
 export const useFetchCoins = () => {
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+
 import { Footer, Header } from "../components";
 
 // eslint-disable-next-line react/prop-types
