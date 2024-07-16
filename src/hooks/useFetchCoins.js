aa71bd91e5e4f3f33048a311cd4a6b8afe0f6aad
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-07-16 01:52:09 -0500

    feat: add use fetch coins hook

---
+++ src/hooks/useFetchCoins.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { getCoins } from "../helpers/getCoins";
+
+export const useFetchCoins = () => {
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getCryptos = async () => {
+    const newCoins = await getCoins();
+    setCoins(newCoins);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getCryptos();
+  }, []);
+
+  return { coins, isLoading };
+};
