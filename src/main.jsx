0bdae4a4c859e0b21bf0cc5c6bb6f94f5fdb4944
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-07-15 20:17:36 -0500

    feat: add dark theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 
 import { CoinApp } from "./CoinApp.jsx";
 
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import darkTheme from "./helpers/theme.js";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CssBaseline />
-    <CoinApp />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <CoinApp />
+    </ThemeProvider>
   </React.StrictMode>
 );
