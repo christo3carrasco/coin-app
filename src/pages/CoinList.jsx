8d27f654fc9471d145d1430f685829609ed36997
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-07-17 18:19:13 -0500

    feat: add coin list

--- src/pages/CoinList.jsx
+++ src/pages/CoinList.jsx
@@ -1,9 +1,28 @@
+import { Box, Typography } from "@mui/material";
+
+import { useFetchCoins } from "../hooks/useFetchCoins";
 import { MainLayout } from "../layouts/MainLayout";
+import { CoinGrid } from "../components";
 
 export const CoinList = () => {
+  const { coins, isLoading } = useFetchCoins();
+
   return (
     <MainLayout>
-      <h1>CoinList</h1>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 4,
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ fontWeight: "bold" }}>
+          Coins
+        </Typography>
+      </Box>
+      {/* eslint-disable-next-line no-undef */}
+      <CoinGrid coins={coins} isLoading={isLoading} />
     </MainLayout>
   );
 };
